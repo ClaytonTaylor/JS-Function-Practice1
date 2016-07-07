3ae430169624558479d8cb0ff73397d00df43360
ClaytonTaylor <claytaylor@cox.net>
2016-07-07 12:22:00 -0600

    Function-Practice1

---
+++ JS-Function-Practice1.js
@@ -0,0 +1,107 @@
+// #1
+
+/*
+function tripleFive(Fives) {
+	for(var i = 0; i < 3; i++ ) 
+		console.log("Five!");
+}
+tripleFive();
+*/
+
+// #2
+
+/*
+
+function lastLetter(String) {
+	console.log(String.substr(String.length-1));
+}
+
+lastLetter("Bacon");
+
+*/
+
+// #3
+
+
+/*
+
+function square(number) {
+	return 3 * number;
+}
+
+square(7)
+
+*/
+
+// #4
+
+/*
+
+function negate(number) {
+	return number * -1;
+}
+
+negate(7)
+
+*/
+
+// #5 
+
+/*
+
+function toArray(par1, par2, par3) {
+	var newArray = [ par1, par2, par3 ]
+	return newArray
+}
+toArray(1, 4, 5);
+
+*/
+
+// #6
+
+/*
+
+function startWithA(String) {
+	if (String.charAt(0) === "a") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+startWithA("apple");
+
+*/
+
+// #7
+
+/*
+
+function excite(String) {
+	return console.log(String + "!!!")
+}
+
+excite("Go")
+
+*/
+
+// #8
+
+/*
+
+function sun(string) {
+	if (string.indexOf("sun")) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+sun("berryflowersun");
+
+*/
+
+// #9
+
